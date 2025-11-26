4107dec24f78d199355f777d01a902364f2a05ea
Usman Yahaya <yahayausman5@gmail.com>
2025-11-27 00:07:53 +0100

    updated views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,26 +1,6 @@
 from django.shortcuts import render, redirect
-from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from .models import Library, Book 
-
-# Create your views here.
-from django.shortcuts import render
-from .models import Book, Library
-
-# Function-based view: list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
-# Class-based view: show details of a specific library
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-
 
 # Login view
 def login_view(request):
@@ -29,7 +9,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('list_books')  # redirect to any page you want
+            return redirect('list_books')
     else:
         form = AuthenticationForm()
     return render(request, 'relationship_app/login.html', {'form': form})
@@ -45,7 +25,7 @@ def register_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # log in immediately after registration
+            login(request, user)
             return redirect('list_books')
     else:
         form = UserCreationForm()

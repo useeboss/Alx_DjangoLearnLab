e4f660c6f80becf4822ee712a9c2fd40524cce26
Usman Yahaya <yahayausman5@gmail.com>
2025-11-27 00:16:24 +0100

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm   
+from django.contrib.auth.forms import UserCreationForm   
 
 
 # Login view

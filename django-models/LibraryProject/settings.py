9c60d0b6ecf66ed3a1fc84b5fe58435207ef48cc
Usman Yahaya <yahayausman5@gmail.com>
2025-11-26 06:56:40 +0100

    updated django project with relationship nested in LibraryProject

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #myapp
-    'relationship_app'
+    #MyApp
+    'LibraryProject.relationship_app'
 ]
 
 MIDDLEWARE = [

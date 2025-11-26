d50baf8846a9bc7a98433be999f46988f9c8d278
Usman Yahaya <yahayausman5@gmail.com>
2025-11-27 00:19:45 +0100

    Fix authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from .views import login_view, logout_view, register_view, list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/',login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/',register_view, name='register'),
+    # Function-based views
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication views
+    path('register/', views.register_view, name='register'),  # checker looks for "views.register"
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]

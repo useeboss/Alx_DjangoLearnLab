396c4f9865bd6185533d4a71e4b3e12a60f206a9
Usman Yahaya <yahayausman5@gmail.com>
2025-11-27 15:49:25 +0100

    added url path

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,16 +13,17 @@ urlpatterns = [
 ]
 
 
+
 urlpatterns = [
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),          
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),  
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
 ]
 
 
 
     # Authentication views
-    path('register/', views.register_view, name='register'),  # checker looks for "views.register"
+    path('register/', views.register_view, name='register'),  
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]

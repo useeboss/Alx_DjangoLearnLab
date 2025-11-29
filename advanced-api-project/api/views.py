e0d65c9630e1d4356322842b473edc48e9c0adf8
Usman Yahaya <yahayausman5@gmail.com>
2025-11-29 23:29:28 +0100

    added new files

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 
 
 
